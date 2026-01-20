940531b92ea49dc63d503ffd05ba5a8c5db85653
Henry Hanlin Zheng <hhzheng1@uci.edu>
2026-01-19 17:24:46 -0800

    List content of a particular directory in given file system

--- a1p1.py
+++ a1p1.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
 def cmd_portal(cmds):
+    '''
+    L - List the contents of the user specified directory.
+    Q - Quit the program.
+    '''
     if cmds[0] == "L":
         L_cmd(cmds)
     elif cmds[0] == "Q":
@@ -10,10 +14,7 @@ def L_cmd(cmds):
     my_path = Path(cmds[1])
     if my_path.exists() and my_path.is_dir():
         for item in my_path.iterdir():
-            if item.is_file():
-                print(f"File: {item.name}")
-            elif item.is_dir():
-                print(f"Directory: {item.name}")
+            print(f"{item}")
     else:
         print("The specified path does not exist or is not a directory.")
 
@@ -21,6 +22,9 @@ def run():
     user_input = input("Enter user command: ")
     cmds = user_input.split(" ")
     cmd_portal(cmds)
+    # print(cmds)
 
 if __name__ == "__main__":
     run()
+
+    # /Users/henryzheng/Documents/UC Irvine/2025-26/WINTER 2026/ICS 32
